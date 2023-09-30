84eb1ac09f83a62cba428c35be47a2fa5bdc6231
KajalPourjalil <rina.skyler@yahoo.com>
2023-09-30 09:48:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let yourArray = ['Kajal', 'Alexandra', 5311, true, 'forever'];
 
 console.log(yourArray);
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 'k';
+// Only change code above this line
+console.log(myArray);
