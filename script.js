647a742a303cbc42ef1a089ef27d11214d9fc035
KajalPourjalil <rina.skyler@yahoo.com>
2023-10-01 12:56:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let yourArray = ['Kajal', 'Alexandra', 5311, true, 'forever'];
+// let yourArray = ['Kajal', 'Alexandra', 5311, true, 'forever'];
 
-console.log(yourArray);
+// // console.log(yourArray);
 
-let myArray = ["a", "b", "c", "d"];
-// Only change code below this line
-myArray[1] = 'k';
-// Only change code above this line
-console.log(myArray);
+// let myArray = ["a", "b", "c", "d"];
+// // Only change code below this line
+// myArray[1] = 'k';
+// // Only change code above this line
+// // console.log(myArray);
 
-function mixedNumbers(arr) {
-    // Only change code below this line
-  arr.unshift('I', 2, 'three');
-  arr.push(7, 'VIII', 9);
+// function mixedNumbers(arr) {
+//     // Only change code below this line
+//   arr.unshift('I', 2, 'three');
+//   arr.push(7, 'VIII', 9);
     // Only change code above this line
-    return arr;
-  }
+    // return arr;
+//   }
   
-  console.log(mixedNumbers(['IV', 5, 'six']));
+//   console.log(mixedNumbers(['IV', 5, 'six']));
 
-  function popShift(arr) {
-    let popped = arr.pop(); // Change this line
-    let shifted = arr.shift(); // Change this line
+function popShift(arr1) {
+    let popped = arr1.pop(); // Change this line
+    let shifted = arr1.shift(); // Change this line
     return [shifted, popped];
-  }
+}
   
   console.log(popShift(['challenge', 'is', 'not', 'complete']));
 
-  
+console.log('hi')
