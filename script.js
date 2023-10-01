bd6e3b0097716af1334d1a6efb204b896ef1a29c
KajalPourjalil <rina.skyler@yahoo.com>
2023-10-01 12:57:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// let yourArray = ['Kajal', 'Alexandra', 5311, true, 'forever'];
+let yourArray = ['Kajal', 'Alexandra', 5311, true, 'forever'];
 
-// // console.log(yourArray);
+console.log(yourArray);
 
-// let myArray = ["a", "b", "c", "d"];
-// // Only change code below this line
-// myArray[1] = 'k';
-// // Only change code above this line
-// // console.log(myArray);
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 'k';
+// Only change code above this line
+console.log(myArray);
 
-// function mixedNumbers(arr) {
-//     // Only change code below this line
-//   arr.unshift('I', 2, 'three');
-//   arr.push(7, 'VIII', 9);
+function mixedNumbers(arr) {
+    // Only change code below this line
+  arr.unshift('I', 2, 'three');
+  arr.push(7, 'VIII', 9);
     // Only change code above this line
-    // return arr;
-//   }
+    return arr;
+  }
   
-//   console.log(mixedNumbers(['IV', 5, 'six']));
+  console.log(mixedNumbers(['IV', 5, 'six']));
 
 function popShift(arr1) {
     let popped = arr1.pop(); // Change this line
