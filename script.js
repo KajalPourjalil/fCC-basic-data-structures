1f420fd2af882e67920237eb9d2dadf7eaa665c1
KajalPourjalil <rina.skyler@yahoo.com>
2023-10-02 18:31:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,12 @@ arr.splice(1,2);
 arr.splice(2,2);
 // Only change code above this line
 console.log(arr);
+
+function htmlColorNames(arr) {
+  // Only change code below this line
+arr.splice(0,2,'DarkSalmon', 'BlanchedAlmond')
+  // Only change code above this line
+  return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
