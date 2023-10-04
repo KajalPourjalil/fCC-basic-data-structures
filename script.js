e63bc9479307d9d7ffd860392b0348386b5d8bf5
KajalPourjalil <rina.skyler@yahoo.com>
2023-10-04 18:44:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,12 @@ arr.splice(0,2,'DarkSalmon', 'BlanchedAlmond')
 }
 
 console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+function forecast(arr) {
+  // Only change code below this line
+arr.slice(2,1);
+  return arr;
+}
+
+// Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
