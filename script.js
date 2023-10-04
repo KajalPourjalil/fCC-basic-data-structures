1b80f57d8a9bb35156342a004a9a83af11e3838e
KajalPourjalil <rina.skyler@yahoo.com>
2023-10-04 18:54:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,17 @@ return arr.slice(2,4);
 
 // Only change code above this line
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // Only change code below this line
+    arr.
+    // Only change code above this line
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
